3180a1c885a98b4cd86c907ebdeb606a64caffd6
anhphuongdo <anhdo.31201024387@st.ueh.edu.vn>
2023-02-25 19:53:09 +0700

    push

---
+++ app/src/main/java/com/example/myapplication/user.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class user {
+    private String name;
+    private String phone_number;
+    private Boolean gender;
+    private String academic_standard;
+    private Short age;
+    private Boolean sport;
+    private String music;
+
+}
